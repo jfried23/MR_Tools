c3803a422454999f3ecb1d8fbaeece9eb781ef8c
Josh Friedman <josh.nmr@gmail.com>
2015-10-07 17:17:46 -0400

    Minor change to support beter data exploration yada yada depends on nothing! :wq

--- src/MRI/MRI_Viewer/CEST_MRI.py
+++ src/MRI/MRI_Viewer/CEST_MRI.py
@@ -1,28 +1,41 @@
 import sys
-sys.path.insert(0, '../')
+import os
+#sys.path.insert(0, '../..')
 
-from MRI import MRI
+import MRI
+
+import ROI
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.widgets import RadioButtons, RectangleSelector
 
-from numpy import sum, shape, squeeze, zeros, zeros_like, amax
+
+import numpy as np
+from numpy import shape, squeeze, zeros, zeros_like, amax
 from matplotlib.pyplot import imshow, plot, show, subplot
+
 
 import pickle
 
-class CEST_MRI( MRI ):
-	def __init__( self, img ):
-		self._radio_dic = {'red':3, 'blue':0, 'green':1,'black':2}
+class CEST_MRI( MRI.MRI ):
+	def __init__( self, img ):		
+
+		self._radio_dic = {'red':3, 'blue':0, 'green':1,' black':2}
 		self._c_indx = 0
-		self._cest_spec = zeros( (4, shape(img)[0]), dtype='float')
+		self.cest = zeros( (4, shape(img)[0]), dtype='float')
+
+		self.box_draw = {}
+		self.select_map = {}
 
 		self._ref_img = zeros_like( img )
 		ref =  amax( img, axis=0 ) 
 		for i, d in enumerate(img ):
 			self._ref_img[i] = img[i]/ref	
 		super(CEST_MRI, self).__init__( squeeze(img) )
+
+	def __del__(self):
+		self.dump_data()
 
 	def _display_image( self ):
 		gs = gridspec.GridSpec( 5, 10)
@@ -41,30 +54,73 @@ class CEST_MRI( MRI ):
                              spancoords='pixels')
 
 		super(CEST_MRI, self)._display_image( self.ax_main )
-			
+	
+	def eval_mask( self, msk = None ):
+		"""
+		INPUT a masked array of the same size as the last two dimensions
+		of the CEST dataset
+
+		Returns the averaged CEST signal over all points in the masked array	
+		"""
+		if msk == None: msk = self.select_map[  self._c_indx ]
+		
+		thr_msk = self._ref_img * msk.mask
+		v = ( thr_msk.mean( axis=-2).mean(axis = -1 ))
+		self.cest[ self._c_indx] = v
+
+		return v	
+		
 	def _sel_data_set(self, lable ):
 		self._c_indx = self._radio_dic[ lable ]	
 
 	def _selector_callback(self,eclick, erelease):
+		self.ax_main.clear()
+		super(CEST_MRI, self)._display_image( self.ax_main )
     		x1, y1 = eclick.xdata, eclick.ydata
     		x2, y2 = erelease.xdata, erelease.ydata
 		
 		dx = [ eclick.xdata, erelease.xdata ]
 		dy = [ eclick.ydata, erelease.ydata ]
+	
+		roi = ROI.ROI( self._ref_img  )
+		rec_patch = roi.rectangle( (x1,y1), x2-x1 , y2-y1 )	
+	
+		self.select_map[  self._c_indx ] = roi
+		self.box_draw[self._c_indx] = rec_patch
 
-		self._cest_spec[ self._c_indx] = sum( self._ref_img[:,min(dy):max(dy), min(dx):max(dx)], axis=(-1,-2) )/(abs(x1-x2)*abs(y1-y2))
+		self.eval_mask()
+			
 		 
 		self.ax_cest.clear()
-		for i, spec in enumerate(self._cest_spec):
+		for i, spec in enumerate(self.cest):
 			k = { 3:'r', 0:'b', 1:'g', 2:'k'}[i]
 			self.ax_cest.plot( spec, k )
+
+
+		self.ax_main.clear()
+		super(CEST_MRI, self)._display_image( self.ax_main )
+		kc = { 3:'r', 0:'b', 1:'g', 2:'k'}
+
+		for k in self.select_map.keys():
+			s= self.box_draw[k]
+			s.set_ec( kc[k] )
+			self.ax_main.add_patch( s )
 		self._fig.canvas.draw()
-	
+
+
+		self._selector = RectangleSelector(self.ax_main, self._selector_callback,
+                             drawtype='box', useblit=False,
+                             button=[1], # don't use middle button
+                             minspanx=5, minspany=5,
+                             spancoords='pixels')
+
 		self.dump_data()
 
+	
 	def dump_data( self, name='./cest_data.p'):
+
 		f = open( name, "wb" )
-		pickle.dump( self._cest_spec, f )
+		pickle.dump( {'cest':self.cest,'sel':self.select_map}, f )
 		f.close()
 
 		
