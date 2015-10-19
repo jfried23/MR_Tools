041df725300fb891dfe278a25860965b14b9e814
Josh Friedman <josh.nmr@gmail.com>
2015-10-19 12:27:27 -0400

    Added support for using display_siemens.py to view datasets contained with hdf5 binaries.

--- src/apps/display_siemens.py
+++ src/apps/display_siemens.py
@@ -6,10 +6,14 @@ sys.path.append( os.path.split( _file_path )[0] )
 
 
 import argparse
+import h5py
 import FileIO.SiemensReader
 import MRI.MRI_Viewer.CEST_MRI
 import MRI.MRI_Viewer.MRI
 
+def print_attrs(name, obj):
+	if isinstance( obj, h5py._hl.group.Group ): return None
+	else: print obj.name
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Display Siemens MRI datasets.')
@@ -19,12 +23,24 @@ if __name__ == '__main__':
 	parser.add_argument('-c', '--cest', default= False, action='store_true',
 			    help='This is a cest dataset True/False.')
 
+	parser.add_argument('-r', '--raw', default= False, action='store_true',
+			    help='This is time domain data True/False.')
+
+
 
 	
 	args = parser.parse_args()
 	
-	data = FileIO.SiemensReader.read_siemens( os.path.abspath( args.file[0] ) )
+	if args.raw:
+		data = FileIO.SiemensReader.read_siemens( os.path.abspath( args.file[0] ) )
+	else:
+		f= h5py.File( os.path.abspath( args.file[0] ) )
+		print 'Avialable files in this repository:'
 
+		f.visititems(print_attrs)		
+
+		a = raw_input("\nEnter the file name: ")
+		data = f[a][...]
 	
 	if args.cest:
 		MRI.MRI_Viewer.CEST_MRI.CEST_MRI( data )
